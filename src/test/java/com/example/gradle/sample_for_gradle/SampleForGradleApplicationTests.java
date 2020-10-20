4e045422b47180697903df8b814a98a98f08f5c5
ChunKang <ale.kang@gmail.com>
2020-10-20 16:37:14 +0800

    Create sample spring project for gradle

---
+++ src/test/java/com/example/gradle/sample_for_gradle/SampleForGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle.sample_for_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleForGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
